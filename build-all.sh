8f9ddb44422240909ee10fb429210b45dc85d0b6
Kosh <koshatul@users.noreply.github.com>
2019-11-05 13:55:30 +1000

    wrapped grep in set +e/-e

--- build-all.sh
+++ build-all.sh
@@ -6,7 +6,9 @@ DOCKER_TAGS="$(curl -sSL 'https://hub.docker.com/v2/repositories/koshatul/curato
 CURATOR_TAGS="$(curl -sSL 'https://api.github.com/repos/elastic/curator/releases')"
 for TAG in $(echo "${CURATOR_TAGS}" | jq -r '.[].tag_name'); do
     echo "## Checking version ${TAG}"
+    set +e
     echo ${DOCKER_TAGS} | jq -r '.results[].name' | grep -q "${TAG}"
+    set -e
     if [[ ${?} != 0 ]]; then
         echo "++ Building version ${TAG}"
         ./build.sh "${TAG}"
