8c63def81bc4e930e72bb3835be2369c53c8f4ca
Kosh <koshatul@users.noreply.github.com>
2019-11-05 14:43:08 +1000

    issue with page_size for docker hub requests

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-DOCKER_TAGS="$(curl -sSL 'https://hub.docker.com/v2/repositories/koshatul/curator/tags')"
+DOCKER_TAGS="$(curl -sSL 'https://hub.docker.com/v2/repositories/koshatul/curator/tags?page_size=1000')"
 CURATOR_TAGS="$(curl -sSL 'https://api.github.com/repos/elastic/curator/releases')"
 for TAG in $(echo "${CURATOR_TAGS}" | jq -r '.[].tag_name' | tac); do
     echo "## Checking version ${TAG}"
@@ -11,7 +11,7 @@ for TAG in $(echo "${CURATOR_TAGS}" | jq -r '.[].tag_name' | tac); do
         echo "++ Building version ${TAG}"
         set -e
         ./build.sh "${TAG}"
-        ./push.sh "${TAG}"
+        # ./push.sh "${TAG}"
         set +e
     fi
 done
