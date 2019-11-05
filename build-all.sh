29b5c33267d73d97ed5e723f904ea5435525b201
Kosh <koshatul@users.noreply.github.com>
2019-11-05 13:56:41 +1000

    more moving set commands around

--- build-all.sh
+++ build-all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-set -eo pipefail
+set -o pipefail
 
 DOCKER_TAGS="$(curl -sSL 'https://hub.docker.com/v2/repositories/koshatul/curator/tags')"
 CURATOR_TAGS="$(curl -sSL 'https://api.github.com/repos/elastic/curator/releases')"
 for TAG in $(echo "${CURATOR_TAGS}" | jq -r '.[].tag_name'); do
     echo "## Checking version ${TAG}"
-    set +e
     echo ${DOCKER_TAGS} | jq -r '.results[].name' | grep -q "${TAG}"
-    set -e
     if [[ ${?} != 0 ]]; then
         echo "++ Building version ${TAG}"
+        set -e
         ./build.sh "${TAG}"
         ./push.sh "${TAG}"
+        set +e
     fi
 done
