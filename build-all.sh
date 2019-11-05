7aaa390629c157f8edf4b932c317c933fbe900ef
Kosh <koshatul@users.noreply.github.com>
2019-11-05 13:34:26 +1000

    missing build-all.sh script

---
+++ build-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_TAGS="$(curl -sSL 'https://hub.docker.com/v2/repositories/koshatul/curator/tags')"
+CURATOR_TAGS="$(curl -sSL 'https://api.github.com/repos/elastic/curator/releases')"
+for TAG in $(echo "${CURATOR_TAGS}" | jq -r '.[].tag_name'); do
+    echo "## Checking version ${TAG}"
+    echo ${DOCKER_TAGS} | jq -r '.results[].name' | grep -q "${TAG}"
+    if [[ ${?} != 0 ]]; then
+        echo "++ Building version ${TAG}"
+        ./build.sh "${TAG}"
+        ./push "${TAG}"
+    fi
+done
